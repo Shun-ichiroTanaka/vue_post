7770c2cd5a24a3e1b66831f760549187e1493d64
田中 俊一朗 <ealbalbs809@gmail.com>
2020-07-01 16:33:05 +0900

    BulmaとTailwindCssの追加

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// 初回アクセス時のみLaravel側でリクエストを受けてページを表示し、
+// それ以降はフロント側のVue Routerによってルーティング
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
