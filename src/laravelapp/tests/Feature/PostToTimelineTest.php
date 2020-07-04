e2a13fa685f7ee19a5ed528bfc9e93beef4b283a
田中 俊一朗 <ealbalbs809@gmail.com>
2020-07-04 23:51:04 +0900

    postモデルの作成

---
+++ src/laravelapp/tests/Feature/PostToTimelineTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostToTimelineTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function a_user_can_post_a_text_post()
+    {
+    }
+}
